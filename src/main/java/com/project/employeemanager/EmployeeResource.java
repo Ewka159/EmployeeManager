058431d4c91c5742db610f43daf31cdcc2ba2619
Natalia Frankiewicz <n.frankiewicz@gmail.com>
2021-12-06 16:38:09 +0100

    api resources

---
+++ src/main/java/com/project/employeemanager/EmployeeResource.java
@@ -0,0 +1,51 @@
+package com.project.employeemanager;
+
+import com.project.employeemanager.model.Employee;
+import com.project.employeemanager.service.EmployeeService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeResource {
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee employee1 =    employeeService.addEmployee(employee);
+        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee employee1 =    employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
